ce8df6ed1fde71f70fbda41535a595ede183b16d
Louis-Philippe Viens <2034421@cegepmontpetit.ca>
2024-05-02 11:04:00 -0400

    PROGRES_MIGRATION_V1_3
    il reste le code a faire dans le PlayerControlleur afin de dechiffrer la position du joueur

---
+++ ProjetFinalBD/ProjetFinalBD/Sql_Scripts/V1_3__ChiffrementDeChiffrement.sql
@@ -0,0 +1,47 @@
+use BD1_BengalsCincinnati_TP1
+go
+create MASTER key ENCRYPTION by Password = 'P4ssword!PasswordETC';
+go
+Create CERTIFICATE MonCertificat with SUBJECT = 'ChiffrementPosition'
+go
+create SYMMETRIC KEY MaCle with ALGORITHM = AES_128 encryption by Certificate MonCertificat;
+go
+
+--Ajout de la colonne
+alter table Players.Player
+add PositionChiffree varbinary(max)
+go
+
+
+
+--Chiffrer les positions des joueurs existants
+open SYMMETRIC key MaCle DECRYPTION by Certificate MonCertificat
+update Players.Player
+set PositionChiffree = EncryptByKey(KEY_GUID('MaCle'), Position)
+close SYMMETRIC key MaCle
+go
+alter table Players.Player
+drop column Position
+go
+
+
+--CREATION de la table pour dechiffrement
+Create Table Players.DeChiffrePosition(
+Position nvarchar(50)
+)
+go
+
+--DECHIFFREMENT DE POSITION
+Create Procedure DeChiffrementPositionDesJoueurs
+	@PlayerID int
+as 
+begin
+	open SYMMETRIC key MaCle DECRYPTION by Certificate MonCertificat
+
+	select Convert(nvarchar(50),DECRYPTBYKEY(PositionChiffree)) as 'Position' from Players.Player
+	where PlayerID = @PlayerID
+	close SYMMETRIC key MaCle
+end
+go
+
+
